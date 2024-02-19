cc073b5d2a945584e5e4a82de43c39c0c6f51e55
mistrZerald <64913419+mistrZerald@users.noreply.github.com>
2024-02-19 02:31:40 +0100

    Add files via upload

--- drone_project.py
+++ drone_project.py
@@ -4,9 +4,9 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist, Pose
 
 """ 
-Wybrany znak:
-Pętla:
-Płaszczyzna:
+Wybrany znak: 'M'
+Pętla: brak
+Płaszczyzna: 
 """
 
 
@@ -27,24 +27,60 @@ class DroneController(Node):
         self.command_pub = self.create_publisher(Twist, '/drone/cmd_vel', 10)
         
         # Callback for executing a control commands
-        timer_period = 0.1  # seconds
+        timer_period = 1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
         # Feel fre to fill with your code! Add some objects to represent a goal points to achieve
-
+	
+	self.Mx1 = 2.0
+	self.My1 = 0.0
+	self.Mx2 = 0.0
+	self.My2 = -1.5
+	
+	self.Mx3 = 2.0
+	self.My3 = -3.0
+	
+	self.Mx4 = 0.0
+	self.My4 = -3.0
     
-    def pose_callback(self, data):
+    def pose_callback(self):
         self.gt_pose = data
-        print(f"{data}")
 
     
     def timer_callback(self):
         # HINT: Check a current pose. Use it to check if a drone achieved a desired pose.
         print(f"Current pose: {self.gt_pose}")
-        
+        x = self.gt.pose.position.x
+        y = self.gt_pose.position.y
         # HINT: Use a self.command_pub to publish a command
         # Fill with your code!
         print("Published!")
+        
+
+                
+        if abs(x - self.Mx1) > 0.1 and if abs(y - self.My1) > 0.1:
+        	speed = Twist()
+        	speed.linear.x = self.goalx1
+        	speed.linear.y = self.goaly1
+                self.command_pub.publish(msg)
+        if abs(x - self.Mx2) > 0.1 and if abs(y - self.My2) > 0.1:
+        	speed = Twist()
+        	speed.linear.x = self.goalx2
+        	speed.linear.y = self.goaly2
+                self.command_pub.publish(msg)
+        if abs(x - self.Mx3) > 0.1 and if abs(y - self.My3) > 0.1:
+        	speed = Twist()
+        	speed.linear.x = self.goalx3
+        	speed.linear.y = self.goaly3
+                self.command_pub.publish(msg)
+        if abs(x - self.Mx4) > 0.1 and if abs(y - self.My4) > 0.1:
+        	speed = Twist()
+        	speed.linear.x = self.goalx4
+        	speed.linear.y = self.goaly4
+	        self.command_pub.publish(msg)
+	
+	
+
 
 
 
